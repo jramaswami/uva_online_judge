4baa9e37ea4edde637822fe2d1b322915c738d00
jramaswami <jramaswami@gmail.com>
2017-05-31 14:23:32 -0400

    11507: python solution AC.

--- 11507/python/uva11507.py
+++ 11507/python/uva11507.py
@@ -14,12 +14,10 @@ def main():
             break
         bends = sys.stdin.readline().split()
         current_direction = "+x"
-        print(bends)
         for bend in bends:
             if bend == 'No':
                 continue
             if current_direction[1] == 'x':
-                print('Changing direction from', current_direction, 'to', end=" ")
                 if current_direction[0] == '-':
                     # next direction is in opposite direction of bend
                     if bend[0] == '-':
@@ -28,18 +26,14 @@ def main():
                         current_direction = '-' + bend[1]
                 else:
                     current_direction = bend
-                print(current_direction)
 
             elif bend[1] != current_direction[1]:
-                print('Bend', bend, 'does not change direction from', current_direction)
                 continue
             else:
-                print('Changing direction from', current_direction, 'to', end=" ")
                 if bend[0] == current_direction[0]:
                     current_direction = "-x"
                 else:
                     current_direction = "+x"
-                print(current_direction)
 
         print(current_direction)
 
