d5836a8989317ad5552408963f617d67ba044ccf
Joseph Ramaswami <jramaswami@gmail.com>
2020-11-01 21:11:44 -0500

    AC

---
+++ 1230/modex.cc
@@ -0,0 +1,35 @@
+// UVA :: 1230 :: Modex
+// https://onlinejudge.org/external/12/1230.pdf
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+// Raise base to the power of exp modulo mod.
+// REF: https://cp-algorithms.com/algebra/binary-exp.html
+ll modpow(ll base, ll exp, ll mod) {
+    ll result{1};
+    while (exp > 0) {
+        if (exp & 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testCases;
+    cin >> testCases;
+    for (int t{0}; t < testCases; ++t) {
+        ll base, exp, mod;
+        cin >> base >> exp >> mod;
+        cout << modpow(base, exp, mod) << "\n";
+    }
+    return EXIT_SUCCESS;
+}
