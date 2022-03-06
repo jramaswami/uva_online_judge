15e6d3482f9e5a05c8351371bf2ed1eb321afe04
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-06 11:33:53 -0500

    WA

---
+++ 1061/blood.py
@@ -0,0 +1,100 @@
+"""
+UVA :: 1061 :: Consanguine Calculations
+https://onlinejudge.org/external/10/1061.pdf
+jramaswami
+
+
+TODO: Fix sorting!
+"""
+
+
+import sys
+import collections
+
+
+COMBINATIONS = [
+    ('A+', 'AA++'), ('A+', 'AA+-'), ('A+', 'AO++'), ('A+', 'AO+-'),
+    ('A-', 'AA--'), ('A-', 'AO--'),
+    ('AB+', 'AB++'), ('AB+', 'AB+-'),
+    ('AB-', 'AB--'),
+    ('B+', 'BB++'), ('B+', 'BB+-'), ('B+', 'BO++'), ('B+', 'BO+-'),
+    ('B-', 'BB--'), ('B-', 'BO--'),
+    ('O+', 'OO++'), ('O+', 'OO+-'),
+    ('O-', 'OO--')
+]
+
+ALL_ALLELES = list(sorted(set(a for _, a in COMBINATIONS)))
+
+def main():
+    "Main program."
+
+    def possible_alleles(blood_type):
+        return [a for bt, a in COMBINATIONS if bt == blood_type]
+
+    def possible_blood_types(allele):
+        return [bt for bt, a in COMBINATIONS if a == allele]
+
+    def combine_alleles(p1, p2):
+        p1abo1, p1abo2, p1rh1, p1rh2 = p1
+        p2abo1, p2abo2, p2rh1, p2rh2 = p2
+
+        for abo1 in [p1abo1, p1abo2]:
+            for abo2 in [p2abo1, p2abo2]:
+                for rh1 in [p1rh1, p1rh2]:
+                    for rh2 in [p2rh1, p2rh2]:
+                        callele = [
+                            min(abo1, abo2), max(abo1, abo2),
+                            min(rh1, rh2), max(rh1, rh2)
+                        ]
+                        yield "".join(callele)
+
+    def bt_key(bt):
+        return (len(bt), bt)
+
+    case = 1
+    while 1:
+
+        line = sys.stdin.readline().strip()
+        if not line:
+            continue
+        if line == 'E N D':
+            break
+
+        parent1, parent2, child = line.split()
+        if parent1 == '?' or parent2 == '?':
+            swapped = False
+            if parent1 == '?':
+                swapped = True
+                parent1, parent2 = parent2, parent1
+
+            result = set()
+            for p1a in possible_alleles(parent1):
+                for p2a in ALL_ALLELES:
+                    for ca in combine_alleles(p1a, p2a):
+                        # print(p1a, p2a, ca, list(possible_blood_types(ca)))
+                        if child in possible_blood_types(ca):
+                            result.update(possible_blood_types(p2a))
+            parent2 = "IMPOSSIBLE"
+            if result:
+                S = ", ".join(t for t in result)
+                parent2 = "{" + S + "}"
+            if swapped:
+                parent1, parent2 = parent2, parent1
+        else:
+            result = set()
+            for p1a in possible_alleles(parent1):
+                for p2a in possible_alleles(parent2):
+                    for ca in combine_alleles(p1a, p2a):
+                        result.update(possible_blood_types(ca))
+            child = "IMPOSSIBLE"
+            if result:
+                S = ", ".join(t for t in result)
+                child = "{" + S + "}"
+        print(f"Case {case}: {parent1} {parent2} {child}")
+
+
+        case += 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ 1061/test.sh
@@ -0,0 +1,27 @@
+exec=$(basename *.cc .cc)
+# echo "Found $exec.cc"
+# echo "Will run ./$exec ..."
+echo "Will run blood.py ..."
+for i in {1..100}
+do
+    if test -f "test$i.txt"; then
+        echo "Running test $i ..."
+        # ./$exec < test$i.txt > output$i.txt
+        python3 blood.py < test$i.txt > output$i.txt
+        # diff -w -s -B expected$i.txt output$i.txt
+        # diff -s expected$i.txt output$i.txt
+        python3 check.py expected$i.txt output$i.txt
+    fi
+done
+
+# for i in {1..10}
+# do
+#     echo "Running random test $i ..."
+#     python3 make_test.py > test_random$i.txt
+#     ./$exec < test_random$i.txt > output_random$i.txt
+#     diff -w -s -B expected_random$i.txt output_random$i.txt
+#     if [ $? -ne 0 ]; then
+#         echo "FAILED TEST"
+#         break
+#     fi
+# done
