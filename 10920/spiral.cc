4651b26de791a001254a34d575431d7627cb9bb1
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-09 15:04:55 -0500

    AC

--- 10920/spiral.cc
+++ 10920/spiral.cc
@@ -6,73 +6,83 @@
 
 using namespace std;
 
-const vector<pair<int, int>> OFFSETS{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+typedef long long int number_t;
 
-struct Matrix {
-    int matrixSize{0}, center{0}, cellCount{0}, currRow{0}, currCol{0},
-        currIndex{0}, targetIndex{0}, currDirn{0}, currSteps{1};
-
-    Matrix(int ms, int ti) {
-        targetIndex = ti;
-        matrixSize = ms;
-        // Determine center of matrix.  Constraints say matrixSize is an
-        // odd number less than 100,000.
-        center = matrixSize / 2;
-        // There are matrixSize^2 cells.
-        cellCount = matrixSize*matrixSize;
-    }
-
-    bool inbounds(int r, int c) {
-        return r >= 0 && r < matrixSize && c >= 0 && c < matrixSize;
-    }
-
-    void move(const pair<int, int> &dirn) {
-        if (currIndex < targetIndex) {
-            currRow += dirn.first;
-            currCol += dirn.second;
-            currIndex++;
-        }
-    }
-
-    void printStatus(ostream &os) {
-        os << "(" << currRow << ", " << currCol << ") @ " << currIndex << endl;
-    }
-
-    pair<int, int> find() {
-        // Put our current position in the center.
-        currRow = center; currCol = center;
-        currIndex = 1; currDirn = 0; currSteps = 1;
-        while (currIndex < targetIndex) {
-            for (int i{0}; i < currSteps; ++i) {
-                move(OFFSETS[currDirn]);
-                if (currIndex == targetIndex) return {matrixSize-currRow, currCol+1};
-            }
-            currDirn = (currDirn + 1) % 4;
-            for (int i{0}; i < currSteps; ++i) {
-                move(OFFSETS[currDirn]);
-                if (currIndex == targetIndex) return {matrixSize-currRow, currCol+1};
-            }
-            currDirn = (currDirn + 1) % 4;
-            currSteps++;
-        }
-        // Convert into positive goes up and one based column.
-        return {matrixSize-currRow, currCol+1};
-    }
-};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     // Solution code.
-    // 1, 1, 2, 2, 3, 3, 4, 4, ...
-
-    int matrixSize{0}, targetIndex{0};
+    number_t matrixSize{0}, targetIndex{0};
     cin >> matrixSize >> targetIndex;
     while (matrixSize || targetIndex) {
-        Matrix M(matrixSize, targetIndex);
-        pair<int, int> soln = M.find();
-        cout << "Line = " << soln.first << ", column = " << soln.second << "." << endl;
+
+        // Binary search for the odd number squared that is greater than or
+        // equal to the target.
+        number_t k{matrixSize};
+        number_t high{matrixSize};
+        number_t low{0};
+        while (low <= high) {
+            number_t mid{low + ((high - low) / 2)};
+            number_t oddMid{(mid*2) + 1};
+            number_t midSq{oddMid * oddMid};
+            if (midSq >= targetIndex) {
+                k = min(oddMid, k);
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        number_t kSq{k * k};
+
+        // TODO: figure out distance from center to square row.
+        // The compute the center of the matrix.
+        number_t center{1 + matrixSize / 2};
+
+        // Figure out where k^2 is located in the matrix and the four corners
+        // of its box.
+        number_t sqDist{((k - 1) / 2)};
+        number_t leftCol{center - sqDist};
+        number_t rightCol{center + sqDist};
+        number_t topRow = {center + sqDist};
+        number_t bottomRow = {center - sqDist};
+
+        // Figure out what is in the other four corners.
+        number_t brVal{kSq - k + 1};
+        number_t blVal{brVal - k + 1};
+        number_t tlVal{blVal - k + 1};
+        number_t prevKSq{tlVal - k + 1};
+        assert (((k-2)*(k-2)) == prevKSq);
+
+        number_t solnRow{-1}, solnCol{-1}, delta{0};
+        if (brVal <= targetIndex && targetIndex <= kSq) {
+            // Case 1: brVal < targetIndex <= k^2
+            // The same column as the square number, the right column.
+            delta = kSq - targetIndex;
+            solnRow = topRow - delta;
+            solnCol = rightCol;
+        } else if (blVal < targetIndex && targetIndex <= brVal) {
+            // Case 2: blVal < targetIndex <= brVal
+            // The bottom row.
+            solnRow = bottomRow;
+            delta = brVal - targetIndex;
+            solnCol = rightCol - delta;
+        } else if (tlVal < targetIndex && targetIndex <= blVal) {
+            // Case 3: tlVal < targetIndex <= blVal
+            // The left column.
+            solnCol = leftCol;
+            delta = blVal - targetIndex;
+            solnRow = bottomRow + delta;
+        } else if (prevKSq < targetIndex && targetIndex <= tlVal) {
+            // Case 4: prevKSq < targetIndex <= tlVal
+            // The same row as the square number, the top column.
+            solnRow = topRow;
+            delta = tlVal - targetIndex;
+            solnCol = leftCol + delta;
+        }
+
+        cout << "Line = " << solnRow << ", column = " << solnCol << "." << endl;
         cin >> matrixSize >> targetIndex;
     }
 
