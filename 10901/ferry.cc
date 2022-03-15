f7c4d355e6405206eb52882d1c934d1721d24caa
Joseph Ramaswami <jramaswami@gmail.com>
2022-03-15 16:14:45 -0400

    AC

--- 10901/ferry.cc
+++ 10901/ferry.cc
@@ -6,11 +6,110 @@
 
 using namespace std;
 
+const int LEFT{0};
+const int RIGHT{1};
+const int WAIT{2};
+const vector<string> BANK_NAME{"Left", "Right"};
+
 struct Car {
-    size_t id;
-    int queueTime;
+    size_t id{0};
+    int queueTime{0};
 
     Car(size_t i, int q): id{i}, queueTime{q} { }
+};
+
+struct Ferry {
+    size_t capacity{0};
+    int crossingTime{0};
+    int clock{0};
+    int bank{LEFT};
+    vector<Car> hold;
+
+    Ferry() { }
+    Ferry(size_t c, int t): capacity{c}, crossingTime{t} { }
+
+    void load(deque<Car> &bank) {
+        while (hold.size() < capacity && !bank.empty() && bank.front().queueTime <= clock) {
+            hold.push_back(bank.front());
+            bank.pop_front();
+        }
+    }
+
+    void unload(vector<int> &arrivalTimes) {
+        for (auto car: hold) {
+            arrivalTimes[car.id] = clock;
+        }
+        hold.clear();
+    }
+
+    void waitUntil(int time) {
+        if (time > clock) {
+            clock = time;
+        }
+    }
+
+    void cross() {
+        clock += crossingTime;
+        bank = (bank == LEFT ? RIGHT : LEFT);
+    }
+
+};
+
+ostream& operator<<(ostream &os, const Ferry &ferry) {
+    os << "Ferry on " << BANK_NAME[ferry.bank] << " clock " << ferry.clock;
+    return os;
+}
+
+struct FerrySystem {
+    Ferry ferry;
+    vector<deque<Car>> banks;
+    vector<int> arrivalTimes;
+
+    FerrySystem(size_t ferryCapacity, int crossingTime, size_t carCount) {
+        banks = vector<deque<Car>>(2, deque<Car>());
+        ferry = Ferry(ferryCapacity, crossingTime);
+        arrivalTimes = vector<int>(carCount, 0);
+    }
+
+    void enqueueLeft(size_t id, int queueTime) {
+        banks[LEFT].emplace_back(id, queueTime);
+    }
+
+    void enqueueRight(size_t id, int queueTime) {
+        banks[RIGHT].emplace_back(id, queueTime);
+    }
+
+    int nextBank() {
+        // Which bank has the next car arriving in line.
+        if (banks[LEFT].empty() && banks[RIGHT].empty()) {
+            return 0;
+        } else if (banks[LEFT].empty()) {
+            return RIGHT;
+        } else if (banks[RIGHT].empty()) {
+            return LEFT;
+        } else if (banks[LEFT].front().queueTime < banks[RIGHT].front().queueTime) {
+            return LEFT;
+        } else if (banks[LEFT].front().queueTime > banks[RIGHT].front().queueTime) {
+            return RIGHT;
+        }
+        return WAIT;
+    }
+
+    void simulate() {
+        while (!(banks[LEFT].empty() && banks[RIGHT].empty())) {
+            if (banks[ferry.bank].empty() || banks[ferry.bank].front().queueTime > ferry.clock) {
+                int nb = nextBank();
+                if (nb == WAIT) {
+                    ferry.waitUntil(banks[ferry.bank].front().queueTime);
+                } else {
+                    ferry.waitUntil(banks[nb].front().queueTime);
+                }
+            }
+            ferry.load(banks[ferry.bank]);
+            ferry.cross();
+            ferry.unload(arrivalTimes);
+        }
+    }
 };
 
 ostream& operator<<(ostream &os, const Car car) {
@@ -30,91 +129,22 @@ int main() {
         size_t ferryCapacity{0}, carCount{0};
         int crossingTime{0};
         cin >> ferryCapacity >> crossingTime >> carCount;
+        FerrySystem system(ferryCapacity, crossingTime, carCount);
         getline(cin, line); // Read trailing newline.
-        deque<Car> leftQueue;
-        deque<Car> rightQueue;
-        vector<int> arrivalTimes;
         for (size_t i{0}; i < carCount; i++) {
             getline(cin, line);
             auto it = find(line.begin(), line.end(), ' ');
             int queueTime{stoi(line.substr(0, distance(line.begin(), it)))};
             string side(it+1, line.end());
             if (side == "left") {
-                leftQueue.emplace_back(i, queueTime);
+                system.enqueueLeft(i, queueTime);
             } else {
-                rightQueue.emplace_back(i, queueTime);
-            }
-            arrivalTimes.push_back(0);
-        }
-
-        bool onLeftBank{true};
-        vector<Car> ferry;
-        int time{0};
-        while (!(leftQueue.empty() && rightQueue.empty())) {
-            cerr << "Time = " << time <<  " on " << (onLeftBank ? "left bank" : "right bank") << endl;
-            cerr << "L: "; copy(leftQueue.begin(), leftQueue.end(), ostream_iterator<Car>(cerr, " ")); cerr << endl;
-            cerr << "R: "; copy(rightQueue.begin(), rightQueue.end(), ostream_iterator<Car>(cerr, " ")); cerr << endl;
-            cerr << endl;
-
-            if (onLeftBank) {
-                // Load waiting cars.
-                while(!leftQueue.empty() && leftQueue.front().queueTime <= time && ferry.size() < ferryCapacity) {
-                    ferry.push_back(leftQueue.front());
-                    leftQueue.pop_front();
-                }
-            } else {
-                // Load waiting cars.
-                while(!rightQueue.empty() && rightQueue.front().queueTime <= time && ferry.size() < ferryCapacity) {
-                    ferry.push_back(rightQueue.front());
-                    rightQueue.pop_front();
-                }
-            }
-
-            // If there are no cars on the ferry wait until one arrives.
-            // If there are cars, cross and deliver the cars.
-            if (ferry.empty()) {
-                if (onLeftBank) {
-                    if (leftQueue.empty()) {
-                        // Cross
-                        cerr << "Crossing to right bank empty." << endl;
-                        time = max(time, rightQueue.front().queueTime) + crossingTime;
-                        onLeftBank = !onLeftBank;
-                    } else if (!rightQueue.empty() && rightQueue.front().queueTime < leftQueue.front().queueTime) {
-                        cerr << "Crossing to right bank empty to pick up car." << endl;
-                        time = rightQueue.front().queueTime + crossingTime;
-                        onLeftBank = !onLeftBank;
-                    } else {
-                        time = leftQueue.front().queueTime;
-                        cerr << "Waiting on left bank until " << time << endl;
-                    }
-                } else {
-                    if (rightQueue.empty()) {
-                        // Cross
-                        cerr << "Crossing to left bank empty." << endl;
-                        time = max(time, leftQueue.front().queueTime) + crossingTime;
-                        onLeftBank = !onLeftBank;
-                    } else if (!leftQueue.empty() && leftQueue.front().queueTime < rightQueue.front().queueTime) {
-                        cerr << "Crossing to left bank empty to pick up car." << endl;
-                        time = leftQueue.front().queueTime + crossingTime;
-                        onLeftBank = !onLeftBank;
-                    } else {
-                        time = rightQueue.front().queueTime;
-                        cerr << "Waiting on right bank until " << time << endl;
-                    }
-                }
-            } else {
-                time += crossingTime;
-                cerr << "Arriving @ " << time << ": ";
-                copy(ferry.begin(), ferry.end(), ostream_iterator<Car>(cerr, ", "));
-                cerr << endl;
-                for (auto car : ferry) {
-                    arrivalTimes[car.id] = time;
-                }
-                ferry.clear();
-                onLeftBank = !onLeftBank;
+                system.enqueueRight(i, queueTime);
             }
         }
-        copy(arrivalTimes.begin(), arrivalTimes.end(), ostream_iterator<int>(cout, "\n"));
+
+        system.simulate();
+        copy(system.arrivalTimes.begin(), system.arrivalTimes.end(), ostream_iterator<int>(cout, "\n"));
         --testCases;
         // Line *between* test cases.
         if (testCases) {
