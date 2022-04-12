f1035bed53fdbff36f06f7ae1b51dd346fcc49b5
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-12 17:02:36 -0400

    AC

--- 11236/grocery.cc
+++ 11236/grocery.cc
@@ -5,33 +5,52 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using number_t = long long int;
+const number_t OFFSET{1000000};
 
-const double EPS{10e-9};
+number_t computeD(number_t a, number_t b, number_t c) {
+    number_t s = (a + b + c) * OFFSET;
+    number_t p = (a * b * c) - OFFSET;
+    if (p > 0 && s % p == 0) {
+        return s / p;
+    }
+    return 0;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
     // Solution code.
-    double a{0.01};
+    number_t a{1};
     int solns{0};
-    while (a <= 20.00) {
-        double b{a};
-        while (a + b <= 20.00) {
-            double c{b};
-            while (a + b + c <= 20.00) {
-                double d{(a + b + c) / ((a * b * c) - 1.00)};
-                if (d >= c && a + b + c + d <= 20.00 && fabs((a+b+c+d) - (a*b*c*d)) < EPS) {
-                    solns++;
-                    /* printf("%0.2f %0.2f %0.2f %0.2f\n", a, b, c, d); */
-                    printf("%0.2f %0.2f %0.2f %0.2f %f %f\n", a, b, c, d, a+b+c+d, a*b*c*d);
+    while (a <= 2000) {
+        number_t b{a};
+        while (a + b <= 2000) {
+            number_t c{b};
+            while (a + b + c <= 2000) {
+                number_t d = computeD(a, b, c);
+                if (d >= c) {
+                    number_t s = (a + b + c + d);
+                    number_t p = (a * b * c * d) / OFFSET;
+                    if (s <= 2000 && s == p) {
+                        solns++;
+                        double a0{static_cast<double>(a) / 100.0};
+                        double b0{static_cast<double>(b) / 100.0};
+                        double c0{static_cast<double>(c) / 100.0};
+                        double d0{static_cast<double>(d) / 100.0};
+                        cout << fixed << setprecision(2) << a0 << " "
+                             << fixed << setprecision(2) << b0 << " "
+                             << fixed << setprecision(2) << c0 << " "
+                             << fixed << setprecision(2) << d0 << endl;
+                    }
                 }
-                c += 0.01;
+                c++;
             }
-            b += 0.01;
+            b++;
         }
-        a += 0.01;
+        a++;
     }
-    cerr << solns;
+    /* cerr << solns << endl; */
     return EXIT_SUCCESS;
 }
--- 11236/test.sh
+++ 11236/test.sh
@@ -1,24 +1,5 @@
 exec=$(basename *.cc .cc)
 echo "Found $exec.cc"
 echo "Will run ./$exec ..."
-for i in {1..100}
-do
-    if test -f "test$i.txt"; then
-        echo "Running test $i ..."
-        ./$exec < test$i.txt > output$i.txt
-        # diff -w -s -B expected$i.txt output$i.txt
-        diff -s expected$i.txt output$i.txt
-    fi
-done
-
-# for i in {1..10}
-# do
-#     echo "Running random test $i ..."
-#     python3 make_test.py > test_random$i.txt
-#     ./$exec < test_random$i.txt > output_random$i.txt
-#     diff -w -s -B expected_random$i.txt output_random$i.txt
-#     if [ $? -ne 0 ]; then
-#         echo "FAILED TEST"
-#         break
-#     fi
-# done
+./$exec > output.txt
+diff -s expected.txt output.txt
