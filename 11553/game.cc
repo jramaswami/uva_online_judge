6d78e62c7cb8e6020213466db54af4872b2f7dc8
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-13 16:42:04 -0400

    AC

--- 11553/game.cc
+++ 11553/game.cc
@@ -2,52 +2,40 @@
 // https://onlinejudge.org/external/115/11553.pdf
 // jramaswami
 
+// The order that Alice chooses the rows does not matter.
+// All that matters is which column Bob should choose from
+// each row in order to minimize the number of candies Alice
+// takes from him.
+
 #include <bits/stdc++.h>
 
 using namespace std;
 using grid_t = vector<vector<int>>;
 using row_t = vector<int>;
-using bs_t = bitset<8>;
 
-// Mutually recursive functions declared ahead of time.
-int alice(int acc, bs_t xRows, bs_t xCols, const grid_t &grid);
-int bob(int acc, size_t lastRow, bs_t xRows, bs_t xCols, const grid_t &grid);
-
-int alice(int acc, bs_t xRows, bs_t xCols, const grid_t &grid) {
-    // Base case: there are no more rows to play.
-    if (xRows.count() == grid.size()) {
+int solve0(int acc, size_t r, bitset<8> xcols, const grid_t &grid) {
+    // Base case.
+    if (xcols.count() == grid.size()) {
         return acc;
     }
 
-    // Alice will maximize her score.
-    int score = INT_MIN;
-    // Alice picks a row that is not crossed out.
-    for (size_t r{0}; r < grid.size(); ++r) {
-        if (!xRows.test(r)) {
-            xRows.set(r);
-            score = max(score, bob(acc, r, xRows, xCols, grid));
-            xRows.reset(r);
-        }
-    }
-    return score;
-}
-
-int bob(int acc, size_t lastRow, bs_t xRows, bs_t xCols, const grid_t &grid) {
-    // Bob will minimize his score.
-    int score = INT_MAX;
-    // Bob will pick a column that is not crossed out.
+    int bestScore{INT_MAX};
     for (size_t c{0}; c < grid.size(); ++c) {
-        if (!xCols.test(c)) {
-            xCols.set(c);
-            score = min(score, alice(acc + grid[lastRow][c], xRows, xCols, grid));
-            xCols.reset(c);
+        if (!xcols.test(c)) {
+            xcols.set(c);
+            bestScore = min(
+                bestScore,
+                solve0(grid[r][c] + acc, r + 1, xcols, grid)
+            );
+            xcols.reset(c);
         }
     }
-    return score;
+    return bestScore;
 }
 
 int solve(const grid_t &grid) {
-    return alice(0, 0, 0, grid);
+    bitset<8> xcols;
+    return solve0(0, 0, xcols, grid);
 }
 
 int main() {
