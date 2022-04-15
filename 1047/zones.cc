dd9c68c647145274e112268d1f9f72d19a4d37ec
Joseph Ramaswami <jramaswami@gmail.com>
2022-04-15 15:24:06 -0400

    Still not right.  I think there is undercounting.

--- 1047/zones.cc
+++ 1047/zones.cc
@@ -6,6 +6,54 @@
 
 using namespace std;
 
+// Choose the towers such that choices including tower 1 are preferable. If
+// this rule still leaves room for more than one solution, solutions including
+// tower 2 are preferable, and so on
+int chooseTowers(int left, int right) {
+    // First bit not set in both.
+    int b = __builtin_ffs(left ^ right) - 1;
+    int mask{1 << b};
+    if (mask & left) {
+        return left;
+    }
+    return right;
+}
+
+int computeCustomersServed(int towers, vector<int> towerCustomers, vector<pair<int, int>> overlappingZones) {
+    cerr << "computing " << bitset<20>(towers) << endl;
+    // Inclusion/exclusion principle.
+    int total{0};
+    // First get the sum of towerCustomers[i] where i is in towers.
+    for (size_t i{0}; i < towerCustomers.size(); ++i) {
+        int mask = 1 << i;
+        if (towers & mask) {
+            total += towerCustomers[i];
+        }
+    }
+
+    // Now, add/subtract overlapping zones.
+    for (auto overlap : overlappingZones) {
+        int subset = overlap.first & towers;
+        if (subset == towers) {
+            continue;
+        }
+        int k = __builtin_popcount(subset);
+        // k will tell you how many times this has already been counted.
+        // Check for overflow.
+        if (k > 1) {
+            cerr << "overlap " << bitset<20>(overlap.first) << endl;
+            if (k % 2) {
+                cerr << bitset<20>(subset) << "+" << overlap.second << endl;
+                total += overlap.second;
+            } else {
+                cerr << bitset<20>(subset) << "-" << overlap.second << endl;
+                total -= overlap.second;
+            }
+        }
+    }
+    cerr << "total=" << total << endl;
+    return total;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -17,72 +65,53 @@ int main() {
     while (planned || built) {
         // planned <= 20
         // Areas will at most 20 bits set, one for each tower.
-        vector<int> areas((1 << 21), 0);
-        // Read the total number of customers each tower serves.
-        for (int i{0}; i < planned; ++i) {
-            cin >> areas[(1 << i)];
-        }
-        // Read the overlapping service areas.
+        vector<int> towerCustomers;
+        towerCustomers.reserve(planned);
+        copy_n(istream_iterator<int>(cin), planned, back_inserter(towerCustomers));
+        // Read the overlapping service zones.
         int ca{0};
         cin >> ca;
+        vector<pair<int, int>> overlappingZones(ca);
         for (int i{0}; i < ca; ++i) {
             // Read number of towers in overlapping area.
             // Use bitmask to indicate that the tower is in the area.
-            int ts{0}, overlap{0};
+            int ts{0};
             cin >> ts;
             for (int j{0}; j < ts; ++j) {
                 int t{0};
                 cin >> t;
-                // Convert to zero-based.
-                t--;
-                overlap |= (1 << t);
+                t--;   // Convert to 0-based indexing.
+                overlappingZones[i].first |= (1 << t);
             }
-            // Read how many customers are in the overlapping area.
-            cin >> areas[overlap];
-            // Now for each tower in the overlap, subtract that from the
-            // area served by the tower alone.
-            for (int b{0}; b < planned; ++b){
-                int mask = (1 << b);
-                if (mask & overlap) {
-                    areas[mask] -= areas[overlap];
-                }
+            // Read how many customers are in the overlapping zone.
+            cin >> overlappingZones[i].second;
+        }
+
+        // Compute solution.
+        cerr << "planned " << planned << " build " << built << endl;
+        int limit = (1 << (planned));
+        int maxCustomers{0};
+        int maxTowers{0};
+        for (int towers{0}; towers < limit; ++towers) {
+            if (__builtin_popcount(towers) != built) {
+                continue;
+            }
+            cerr << "popcount " << __builtin_popcount(towers) << endl;
+            int customers = computeCustomersServed(
+                                towers, towerCustomers, overlappingZones
+            );
+            cerr << bitset<20>(towers) << " " << customers << endl;
+            if (customers > maxCustomers) {
+                maxCustomers = customers;
+                maxTowers = towers;
+            } else if (customers == maxCustomers) {
+                maxTowers = chooseTowers(maxTowers, towers);
             }
         }
 
-        for (size_t i{0}; i < areas.size(); ++i) {
-            if (areas[i]) {
-                cerr << bitset<20>(i) << " " << areas[i] << endl;
-            }
-        }
-
-        // Solve
-        int maxServed{0}, maxTowers{0};
-        for (int candidate{0}; candidate <= (1 << (planned+1)); ++candidate) {
-            // Find numbers where the popcount is the number of towers built.
-            if (__builtin_popcount(candidate) == built) {
-                // Find the sum of all the areas served by the towers.
-                int served{0};
-                for (size_t i{0}; i < areas.size(); i++) {
-                    if (i & candidate) {
-                        served += areas[i];
-                    }
-                }
-                if (served > maxServed) {
-                    maxServed = served;
-                    maxTowers = candidate;
-                } else if (served == maxServed) {
-                    // Lexicographic.
-                    // First bit not set in both.
-                    int b = __builtin_ffs(candidate ^ maxTowers) - 1;
-                    int mask{1 << b};
-                    if (mask & candidate) {
-                        maxTowers = candidate;
-                    }
-                }
-            }
-        }
+        // Print solution.
         cout << "Case Number  " << caseNo << endl;
-        cout << "Number of Customers: " << maxServed << endl;
+        cout << "Number of Customers: " << maxCustomers << endl;
         cout << "Locations recommended: ";
         for (int b{0}; b < 32; ++b) {
             int mask = (1 << b);
