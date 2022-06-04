0bc16387939d6233dbf9b66fe04905d4daaff07a
Joseph Ramaswami <jramaswami@gmail.com>
2022-06-04 15:47:20 -0400

    AC

--- 11517/uva11517.cc
+++ 11517/uva11517.cc
@@ -6,18 +6,6 @@
 
 using namespace std;
 
-
-struct QItem {
-    int coinCount, coinSum, lastCoin;
-};
-
-bool operator< (const QItem &lhs, const QItem &rhs) {
-    if (lhs.coinCount == rhs.coinCount) {
-        return lhs.coinSum < rhs.coinSum;
-    }
-    return lhs.coinCount < rhs.coinCount;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -31,26 +19,35 @@ int main() {
         vector<int> coins;
         coins.reserve(N);
         copy_n(istream_iterator<int>(cin), N, back_inserter(coins));
-        priority_queue<QItem> pq;
-        pq.push(QItem{0, 0, -1});
-        int solnAmount{INT_MAX}, solnCount{INT_MAX};
-        while (!pq.empty()) {
-            QItem item = pq.top();
-            pq.pop();
-            if (target <= item.coinSum) {
-                if (item.coinSum == solnAmount) {
-                    solnCount = min(solnCount, item.coinCount);
-                } else if (item.coinSum < solnAmount) {
-                    solnAmount = item.coinSum;
-                    solnCount = item.coinCount;
-                }
-            }
 
-            for (int i{item.lastCoin + 1}; i < N; ++i) {
-                if (item.coinSum <= solnAmount) {
-                    pq.push(QItem{item.coinCount + 1, item.coinSum + coins[i], i});
+        int solnAmount{INT_MAX}, solnCount{INT_MAX};
+        // dp[amount] = min coins
+        vector<int> prevDp(target, INT_MAX);
+        prevDp[0] = 0;
+        vector<int> nextDp(target, INT_MAX);
+        for (auto coin : coins) {
+            for (int prevAmount{0}; prevAmount < target; ++prevAmount) {
+                if (prevDp[prevAmount] < INT_MAX) {
+                    // Do not choose coin.
+                    nextDp[prevAmount] = min(nextDp[prevAmount], prevDp[prevAmount]);
+
+                    // Choose coin.
+                    int nextAmount{prevAmount + coin};
+                    int nextCount{prevDp[prevAmount] + 1};
+                    if (nextAmount < target) {
+                        nextDp[nextAmount] = min(nextDp[nextAmount], nextCount);
+                    } else {
+                        if (nextAmount < solnAmount) {
+                            solnAmount = nextAmount;
+                            solnCount = nextCount;
+                        } else if (nextAmount == solnAmount) {
+                            solnCount = min(solnCount, nextCount);
+                        }
+                    }
                 }
             }
+            prevDp.swap(nextDp);
+            fill(nextDp.begin(), nextDp.end(), INT_MAX);
         }
         cout << solnAmount << " " << solnCount << "\n";
 
