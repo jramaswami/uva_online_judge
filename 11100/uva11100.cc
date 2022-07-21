c1430d7ea449a11860721b642c3a68aea4ae2e6a
Joseph Ramaswami <jramaswami@gmail.com>
2022-07-21 12:00:27 -0400

    AC

--- 11100/uva11100.cc
+++ 11100/uva11100.cc
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+// Find the item with the highest frequency.
+int highestFrequency(vector<int> &A) {
+    int curr{A[0]}, freq{1}, result{0};
+    for (size_t i{1}; i < A.size(); i++) {
+        if (A[i] == curr) {
+            freq++;
+        } else {
+            result = max(result, freq);
+            curr = A[i];
+            freq = 1;
+        }
+    }
+    result = max(result, freq);
+    return result;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -17,21 +33,16 @@ int main() {
         A.reserve(N);
         copy_n(istream_iterator<int>(cin), N, back_inserter(A));
         sort(A.begin(), A.end());
-        deque<vector<int>> Q;
+        int M{highestFrequency(A)};
+        vector<vector<int>> B(M, vector<int>());
+        int i{0};
         for (auto k : A) {
-            if (Q.empty() || Q.front().back() >= k) {
-                vector<int> X{k};
-                Q.push_back(X);
-            } else {
-                vector<int> X{Q.front()};
-                Q.pop_front();
-                X.push_back(k);
-                Q.push_back(X);
-            }
+            B[i].push_back(k);
+            i = (i + 1) % M;
         }
 
-        cout << Q.size() << "\n";
-        for (auto X : Q) {
+        cout << M << "\n";
+        for (auto X : B) {
             for (size_t i{0}; i < X.size(); i++) {
                 cout << X[i];
                 if (i + 1 < X.size()) {
