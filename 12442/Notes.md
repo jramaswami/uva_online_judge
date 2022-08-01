ed0e4cbd32f75099d16ffbfa92a1f06c593e6ed4
Joseph Ramaswami <jramaswami@gmail.com>
2022-08-01 14:08:08 -0400

    AC

---
+++ 12442/Notes.md
@@ -0,0 +1,5 @@
+# Notes for UVA 12442
+
+Test number 10 is not a valid test and therefore fails.  It is invalid because
+node number one does not forward email to anyone.  The problem statement
+requires each node to forward to another, "exactly one, no less, no more."
--- 12442/uva12442.cc
+++ 12442/uva12442.cc
@@ -15,34 +15,68 @@ int main() {
     for (int t{1}; t <= testCases; t++) {
         int nodeCount{0};
         cin >> nodeCount;
-        vector<vector<int>> graph(nodeCount+1, vector<int>());
+        vector<int> graph(nodeCount+1, -1);
         for (int i{0}; i < nodeCount; i++) {
             int u{0}, v{0};
             cin >> u >> v;
-            graph[u].push_back(v);
+            graph[u] = v;
         }
-        // Simplest possible solution.
-        int bestNode{1}, maxChildren{0};
-        for (int r{1}; r <= nodeCount; r++) {
-            deque<bool> visited(nodeCount+1, false);
-            deque<int> queue;
-            queue.push_back(r);
-            visited[r] = true;
-            int children{0};
-            while (!queue.empty()) {
-                int u = queue.front();
-                queue.pop_front();
-                for (int v : graph[u]) {
-                    if (!visited[v]) {
-                        children++;
-                        visited[v] = true;
-                        queue.push_back(v);
+
+        // Find any cycles and cache their size.
+        vector<int> pathIndex(nodeCount + 1, -1);
+        vector<int> cache(nodeCount + 1, -1);
+        for (int u{1}; u <= nodeCount; u++) {
+            if (pathIndex[u] >= 0) {
+                continue;
+            }
+            vector<int> stack;
+            stack.push_back(u);
+            int pi{0};
+            pathIndex[u] = pi;
+            bool ok{true};
+            while (ok) {
+                int v = graph[stack.back()];
+                if (pathIndex[v] >= 0) {
+                    // We have visited this node before.
+                    int p{pathIndex[v]};
+                    if (p <= pi && stack[p] == v) {
+                        // If the node is in the stack, then this is a cycle.
+                        // Cache the length of the cycle for all members of
+                        // cycle, removing them from the stack.
+                        int cycleLength = pi - pathIndex[v] + 1;
+                        while (stack.back() != v) {
+                            cache[stack.back()] = cycleLength;
+                            stack.pop_back();
+                        }
+                        cache[stack.back()] = cycleLength;
+                        stack.pop_back();
+                        ok = false;
+                    } else {
+                        // If the node is not in our stack then we have run
+                        // into a cached node.
+                        ok = false;
                     }
+                    // Any nodes on the stack are connected to a cached node.
+                    while(!stack.empty()) {
+                        // Cache me + the number of children for my child.
+                        cache[stack.back()] = 1 + cache[graph[stack.back()]];
+                        stack.pop_back();
+                    }
+                } else {
+                    stack.push_back(v);
+                    pi++;
+                    pathIndex[v] = pi;
                 }
             }
-            if (children > maxChildren) {
-                maxChildren = children;
-                bestNode = r;
+        }
+
+        // Find the best node.
+        int bestNode{-1};
+        int maxChildren{-1};
+        for (int u{1}; u <= nodeCount; u++) {
+            if (cache[u] > maxChildren) {
+                bestNode = u;
+                maxChildren = cache[u];
             }
         }
         cout << "Case " << t << ": " << bestNode << "\n";
